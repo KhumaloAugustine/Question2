101cc2689fba8ea4b616446fc77dcb10a55a15c0
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-04-11 11:51:07 +0200

    Added comments to RoomType.java for readability

--- src/com/hotelReservationSystem/model/RoomType.java
+++ src/com/hotelReservationSystem/model/RoomType.java
@@ -1,8 +1,12 @@
 package com.hotelReservationSystem.model;
 
 public enum RoomType {
+    // Room can accommodate one guest
     SINGLE,
+    // Room can accommodate two guests
     DOUBLE,
+    // Room can accommodate a king-size bed, typically for two guests
     KING,
+    // Suite offers multiple rooms or a larger space, suitable for more guests
     SUITE
 }
